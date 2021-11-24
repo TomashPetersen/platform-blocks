a256c5c37fd0b57c59a0d6f7018f832316036808
Denis Volkov <d.g.volkovbz@gmail.com>
2021-11-24 16:27:33 +0400

    add drowing all sprites, including blocks

--- game.js
+++ game.js
@@ -1,9 +1,15 @@
 let game =  {
   ctx: null,
+  platform: null,
+  ball: null,
+  blocks: [],
+  rows: 4,
+  cols: 8,
   sprites: {
     background: null,
     ball: null,
     platform: null,
+    block: null
   },
   
   init () {
@@ -15,25 +21,45 @@ let game =  {
     // preload
     let loaded = 0;
     let required = Object.keys(this.sprites).length;
+    let onImageLoad = () => {
+      ++loaded;
+      if (loaded >= required) {
+        callback();
+      }
+    }
 
     for (let key in this.sprites) {
       this.sprites[key] = new Image();
       this.sprites[key].src = `img/${key}.png`;
-      this.sprites[key].addEventListener("load", () => {
-        ++loaded;
-        if (loaded >= required) {
-          callback();
-        }
-      });
+      this.sprites[key].addEventListener("load", onImageLoad);
     }
 
+  },
+
+  renderBlocks () {
+    for (let block of this.blocks) {
+      this.ctx.drawImage(this.sprites.block, block.x, block.y);
+      console.log("block");
+    }
   },
 
   render (){
     // render 
     this.ctx.drawImage(this.sprites.background, 0, 0);
-    this.ctx.drawImage(this.sprites.ball, 0, 0);
-    this.ctx.drawImage(this.sprites.platform, 0, 0);
+    this.ctx.drawImage(this.sprites.ball, 0, 0, this.ball.width, this.ball.height, this.ball.x, this.ball.y, this.ball.width, this.ball.height);
+    this.ctx.drawImage(this.sprites.platform, this.platform.x, this.platform.y);
+    this.renderBlocks ();
+  },
+
+  create () {
+    for (let row = 0; row < this.rows; row++) {
+      for (let col = 0; col < this.cols; col++) {
+        this.blocks.push({
+          x: 64 * col + 65,
+          y: 24 * row + 30,
+        });
+      }
+    }
   },
 
   run () {
@@ -46,12 +72,23 @@ let game =  {
   start () {
     this.init();
     this.preload(() => {
+      this.create();
       this.run();
     });
-
   }
 };
 
+game.platform = {
+  x: 280,
+  y: 300,
+};
+
+game.ball = {
+  x: 320,
+  y: 280,
+  width: 20,
+  height: 20
+};
 
 window.addEventListener("load", () => {
   game.start();
